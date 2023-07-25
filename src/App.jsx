f60fdc6411d9e6b6b368af87168e673243010e57
lexie70 <alexandracampos658@gmail.com>
2023-07-25 13:01:15 -0600

    controlled inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,54 @@ import React, {useState} from 'react';
 import './App.css'
 
 export default function App() {
-  const[name,setName]= useState("")
-  const[email,setEmail]= useState("")
-  const[profileData,setProfileData]= useState([])
-  const handleProfileData= ()=>{
-    
+  const[data,setData]= useState([{
+    name: "name",
+    email:"email"
+  }])
+  const[profileData,setProfileData]= useState([{name:"maria", email:23},{name:"pablo", email:23}])
+ 
+  const handleSubmit= (event)=>{
+    event.preventDefault();
+    setProfileData([{}]);
+    console.log(data);
+    setData({
+      name: "",
+      email:""
+    })
   }
   return (
     
       <div className="App">
-        <form className="myForm">
+        <form className="myForm" >
           <h1>Sign Up</h1>
           <label htmlFor= "name">Enter your name: </label>
           <input type='text'
-          value={name}
-          onChange={(e)=>{setName(e.target.value)}}
+          value={data.name}
+          onChange={(e)=>{setData({...data,name:e.target.value})}}
           id='name'/>
 
           <label htmlFor='email'>Enter your email: </label>
-          <input type='email'
-           value={email}
-           onChange={(e)=>{setEmail(e.target.value)}}
+          <input type='text'
+           value={data.email}
+           onChange={(e)=>{setData({...data, email: e.target.value})}}
            id='email'/>
 
-          <button>Signup!</button>
+          <button type='submit' onClick={handleSubmit}>Signup!</button>
         </form>
         <div className='user-list'>
           <h1>Users:</h1> 
+          <ul>
+            {profileData.map((item,index)=>{
+              return(
+                <li key={index}>User {index+1} : {item.name}</li>       
+                )
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
+
 
     
       
